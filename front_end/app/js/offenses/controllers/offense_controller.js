9e4c84f30e09df3d58990489e99cb518362ad00b
MARIA ROMERO <mariaromero18e@gmail.com>
2016-06-07 12:06:31 -0700

    create news controller, service, router

--- front_end/app/js/offenses/controllers/offense_controller.js
+++ front_end/app/js/offenses/controllers/offense_controller.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.controller('OffenseController', ['sgvdtResource', function(Resource) {
     this.offenses = [];
     this.errors = [];
-    var remote = new Resource(this.offenses, this.errors, baseUrl + '/api/offenses', { errMessages: { getAll: 'custome error message' } });
+    var remote = new Resource(this.offenses, this.errors, baseUrl + '/api/offenses', { errMessages: { getAll: 'custom error message' } });
     this.getAll = remote.getAll.bind(remote);
     this.getAll();
   }]);
